eeedca3bab7808403efbe577b35632d22952f14e
rmrpereira <73123902+rmrpereira@users.noreply.github.com>
2021-05-29 18:31:07 +0100

    Add files via upload

--- test2.py
+++ test2.py
@@ -6,6 +6,16 @@ from matplotlib import pyplot as plt
 numero_control = [0, 0]
 numero_total = [0]
 
+numero_controlred = [0, 0]
+numero_red = [0]
+
+numero_controlyellow = [0, 0]
+numero_yellow = [0]
+
+numero_controlgreen = [0, 0]
+numero_green = [0]
+
+
 def camara():
     cap = cv.VideoCapture(0, cv.CAP_V4L2)
     ret, frame = cap.read()
@@ -117,12 +127,34 @@ def numerototal(control):
     numero_control[1] = control
     if(control > numero_control[0] or numero_total[0]==0):
         numero_total[0] = numero_total[0] + (control-numero_control[0])
-        
+
+def numerototalred(control_red):
+    numero_controlred[0] = numero_controlred[1]    
+    numero_controlred[1] = control_red
+    if(control_red > numero_controlred[0] or numero_red[0]==0):
+        numero_red[0] = numero_red[0] + (control_red-numero_controlred[0])
+
+def numerototalyellow(control_yellow):
+    numero_controlyellow[0] = numero_controlyellow[1]    
+    numero_controlyellow[1] = control_yellow
+    if(control_yellow > numero_controlyellow[0] or numero_yellow[0]==0):
+        numero_yellow[0] = numero_yellow[0] + (control_yellow-numero_controlyellow[0])
+
+def numerototalgreen(control_green):
+    numero_controlgreen[0] = numero_controlgreen[1]    
+    numero_controlgreen[1] = control_green
+    if(control_green > numero_controlgreen[0] or numero_green[0]==0):
+        numero_green[0] = numero_green[0] + (control_green-numero_controlgreen[0])
+
+
 def countrs(arraymask,image):
     array,name = color_range()
     ar = 3500 
 
     control = 0
+    control_red = 0
+    control_yellow = 0
+    control_green = 0
     
     for k in range(len(name)):
         contours, hierarchy = cv.findContours(arraymasks[k], cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -133,16 +165,30 @@ def countrs(arraymask,image):
                 cv.drawContours(image, contours, -1, (0, 255, 0), 5)
                 image = cv.putText(image, name[k], (x, y), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 1, cv.LINE_AA)
                 image = cv.putText(image, str(int(((h))/32))+'x'+str(int(((w))/32)), (x+100, y), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 1, cv.LINE_AA)
-                print(w)
+                #print(w)
                 #print(h)
                 control = control + 1
-    #numero_control[0] = numero_control[1]    
-    #numero_control[1] = control
-    #if(control > numero_control[0] or numero_total[0]==0):
-    #    numero_total[0] = numero_total[0] + (control-numero_control[0]) 
+                if(name[k]=='Red'):
+                    control_red = control_red + 1
+                if(name[k]=='Yellow'):
+                    control_yellow = control_yellow + 1
+                if(name[k]=='Green'):
+                    control_green = control_green + 1
+    
     numerototal(control)
+    numerototalred(control_red)
+    numerototalyellow(control_yellow)
+    numerototalgreen(control_green)
+    print("\nInformação atual")
     print("Numero de peças: "+str(int(control)))
+    print("Numero de peças Vermelhas: "+str(int(control_red)))
+    print("Numero de peças Amarelas: "+str(int(control_yellow)))
+    print("Numero de peças Verdes: "+str(int(control_green)))
+    print("\nHistorico")
     print("Numero total de peças: "+str(int(numero_total[0])))
+    print("Numero total de peças Vermelhas: "+str(int(numero_red[0])))
+    print("Numero total de peças Amarelas: "+str(int(numero_yellow[0])))
+    print("Numero total de peças Verdes: "+str(int(numero_green[0])))
     return  image
 
 #main
